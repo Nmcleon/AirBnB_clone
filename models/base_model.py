cc05b0c300ae73edd2cc87c7df2bd6371808ee08
N <51336368+Nmcleon@users.noreply.github.com>
2023-10-11 13:16:59 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,24 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         This method initializes a BaseModel instance.
         - Sets a unique 'id' for the instance.
         - Sets 'created_at' and 'updated_at' attributes to the current datetime.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+		if kwargs:
+			for key, value in kwargs.items():
+				if key != '__class__':
+					if key == 'created_at' or key == 'updated_at':
+						setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+						setattr(self, key, value)
+		else:
+			self.id = str(uuid.uuid4())
+			self.created_at = datetime.now()
+			self.updated_at = datetime.now()
+			storage.new(self)
 
     def save(self):
         """
