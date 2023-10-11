630fd82702de3b1b8e2992f729742a4e1fde3ffd
N <51336368+Nmcleon@users.noreply.github.com>
2023-10-11 13:28:55 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -10,21 +10,21 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """
         This method initializes a BaseModel instance.
-        - Sets a unique 'id' for the instance.
-        - Sets 'created_at' and 'updated_at' attributes to the current datetime.
+        Sets a unique 'id' for the instance.
+        Sets 'created_at' and 'updated_at' attributes to the current datetime.
         """
-	    if kwargs:
-			for key, value in kwargs.items():
-				if key != '__class__':
-					if key == 'created_at' or key == 'updated_at':
-						setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                     else:
-						setattr(self, key, value)
-		else:
-			self.id = str(uuid.uuid4())
-			self.created_at = datetime.now()
-			self.updated_at = datetime.now()
-			storage.new(self)
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """
@@ -35,8 +35,8 @@ class BaseModel:
     def to_dict(self):
         """
         Returns dictionary representation of BaseModel instance.
-        - Includes 'id', 'created_at', 'updated_at', and '__class__' keys.
-        - 'created_at' and 'updated_at' are converted to ISO format.
+        Includes 'id', 'created_at', 'updated_at', and '__class__' keys.
+        'created_at' and 'updated_at' are converted to ISO format.
         """
         class_name = self.__class__.__name__
         return {
@@ -53,4 +53,3 @@ class BaseModel:
         """
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
-
