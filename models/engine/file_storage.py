489f40e454505ac98b88245dfb0260658e97f60d
N <51336368+Nmcleon@users.noreply.github.com>
2023-10-12 09:32:30 +0200

    Create  and update file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,30 @@
+import json
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.____name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        serialized_data = {}
+        for key, value in FileStorage.__objects.items():
+            serialized_data[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(serialized_data, file)
+
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    obj = models.classes[class_name](**value)
+                    FileStorage.__objects[key] = obj
+        except Exception:
+            pass
