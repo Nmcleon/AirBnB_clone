c57f53e509fdb19a54cc5e82d419bc860d300eee
Nmcleon <nmcleon10@gmail.com>
2023-10-14 20:28:22 +0200

    err

--- console.py
+++ console.py
@@ -54,19 +54,20 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        split_args = args.split()
-        if split_args[0] not in HBNBCommand.__classes:
+        args = args.split()
+        if args[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
             return
-        if len(split_args) == 1:
+        if len(args) == 1:
             print("** instance id missing **")
             return
         all_objs = storage.all()
-        obj_id = "{}.{}".format(split_args[0], split_args[1])
+        obj_id = "{}.{}".format(args[0], args[1])
         if obj_id not in all_objs:
             print("** no instance found **")
             return
-        print(all_objs[obj_id])
+        obj = all_objs[obj_id]
+        print(obj)
         
     def do_destroy(self, args):
         """Deletes an instance based 
@@ -76,15 +77,15 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        split_args = args.split()
-        if split_args[0] not in HBNBCommand.__classes:
+        args = args.split()
+        if args[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
             return
-        if len(split_args) == 1:
+        if len(args) == 1:
             print("** instance id missing **")
             return
         all_objs = storage.all()
-        obj_id = "{}.{}".format(split_args[0], split_args[1])
+        obj_id = "{}.{}".format(args[0], args[1])
         if obj_id not in all_objs:
             print("** no instance found **")
             return
@@ -107,49 +108,65 @@ class HBNBCommand(cmd.Cmd):
         print(result)
 
     def do_update(self, args):
-        """  Update an instance based 
-        on the class name and id
-        """
+        """Updates an instance based on the class name and id."""
         if not args:
             print("** class name missing **")
             return
-        split_args = args.split()
-        if split_args[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-            return
-        if len(split_args) < 2:
-            print("** instance id missing **")
-            return
-        obj_id = "{}.{}".format(split_args[0], split_args[1])
-        all_objs = storage.all()
-        if obj_id not in all_objs:
-            print("** no instance found **")
-            return
-        if len(split_args) < 3:
-            print("** attribute name missing **")
-            return
-        if len(split_args) < 4:
-            print("** value missing **")
-            return
 
-        attribute_name = split_args[2]
-        # Get attribute value
-        attribute_value = split_args[3]
-        if attribute_value.startswitch('"') and attribute_value.endswitch('"'):
-            attribute_value = attribute_value[1:-1]
-        obj_to_update = all_objs[obj_id]
-        if attribute_name != 'id' and attribute_name != 'created_at' and attribute_name != 'updated_at':
-            try:
+    split_args = args.split()
+    class_name = split_args[0]
+
+    if class_name not in HBNBCommand.__classes:
+        print("** class doesn't exist **")
+        return
+
+    if len(split_args) < 2:
+        print("** instance id missing **")
+        return
+
+    obj_id = split_args[1]
+    all_objs = storage.all()
+    obj_key = "{}.{}".format(class_name, obj_id)
+
+    if obj_key not in all_objs:
+        print("** no instance found **")
+        return
+
+    if len(split_args) < 3:
+        print("** attribute name missing **")
+        return
+
+    attribute_name = split_args[2]
+
+    if len(split_args) < 4:
+        print("** value missing **")
+        return
+
+    # Join the attribute value parts into a single string, stripping double quotes
+    attribute_value = " ".join(split_args[3:])[1:-1]
+
+    obj = all_objs[obj_key]
+
+    # Check if the attribute name is valid and not 'id', 'created_at', or 'updated_at'
+    if hasattr(obj, attribute_name) and attribute_name not in ["id", "created_at", "updated_at"]:
+        # Cast attribute value to the appropriate type based on the attribute's type
+        attr_type = type(getattr(obj, attribute_name))
+        try:
+            if attr_type is str:
+                attribute_value = str(attribute_value)
+            elif attr_type is int:
                 attribute_value = int(attribute_value)
-            except ValueError:
-                try:
-                    attribute_value = float(attribute_value)
-                except ValueError:
-                    pass
-                #update obj's attribute
-                setattr(obj_to_update, attribute_name, attribute_value)
-                obj_to_update.save()
+            elif attr_type is float:
+                attribute_value = float(attribute_value)
+        except (ValueError, TypeError):
+            print("** invalid value **")
+            return
 
+        setattr(obj, attribute_name, attribute_value)
+        obj.save()
+    else:
+        print("** attribute doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
