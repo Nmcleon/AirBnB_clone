10d24c3d75bf23b6731a7525a12c23e06b8da743
Nmcleon <nmcleon10@gmail.com>
2023-10-15 08:49:27 +0200

    create and update city.py

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines City Class"""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+	"""Represent A City
+	Attributes:
+		name (str): Name of the city
+		state_id (str): state id
+	"""
+
+	state_id = ""
+	name = ""
