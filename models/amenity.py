b120290db6d914c4f014606c55dd1488ca2b63ea
Nmcleon <nmcleon10@gmail.com>
2023-10-15 09:06:39 +0200

    create and update amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define Amenity class"""
+
+from model.base_model import BaseModel
+
+class Amenity(BaseModel):
+	"""Represent an amenity
+	Attributes:
+		name (str): Name of the amenity
+	"""
+	name = ""
