80f55259961fa430b76a57b30692dbf5d6b6bf22
Nmcleon <nmcleon10@gmail.com>
2023-10-15 09:00:46 +0200

    create and update state.py

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define State class"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+	"""Represent a state
+	Attributes:
+		name (str): name of the state
+	"""
+    name = ""
