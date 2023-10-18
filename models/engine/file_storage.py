699999eea17da027a9155273f2639a1a168ed15e
N <51336368+Nmcleon@users.noreply.github.com>
2023-10-18 12:50:27 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 27 13:56:43 2020
 
-@authors: Robinson Montes
-          Carlos Murcia
-"""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -17,36 +11,25 @@ from models.review import Review
 
 
 class FileStorage:
-    """
-    FileStorage class for serializing and deserializing objects into and from
-    files.
-    """
+    """ class for serializing and deserializing    """
     __file_path = 'file.json'
     __objects = dict()
 
     def __init__(self):
-        """init method for FileStorage class
-        """
+        """init file storage"""
         pass
 
     def all(self):
-        """returns the dictionary __objects
-        """
+        """returns dictionary __objects"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id
-
-        Attributes:
-            obj (Python object): The object to set
-        """
         dictionary = obj.to_dict()
         key = '{}.{}'.format(dictionary['__class__'], str(obj.id))
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)
-        """
+        """serializes __objects to the file.json"""
         dictionary = dict()
         for k, v in FileStorage.__objects.items():
             dictionary[k] = v.to_dict()
@@ -54,10 +37,7 @@ class FileStorage:
             json.dump(dictionary, file)
 
     def reload(self):
-        """deserializes the JSON file to __objects ONLY if the JASON file
-        exists, otherwise, do nothing.  If the file doesn't exist, exceptions
-        should be raised
-        """
+        """deserialize file.json"""
         try:
             with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
                 json_load = json.load(file)
